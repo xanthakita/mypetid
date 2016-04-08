bd511c54f65f96c97c0b415db0bfdea5252facd2
Jonathan Wagner <xanthakita@gmail.com>
2016-04-08 17:10:17 -0400

    removed password
    had password in file inadvertently

--- htdocs/phpwork/mysql_info.php
+++ htdocs/phpwork/mysql_info.php
@@ -1,7 +1,7 @@
 <?php
-	$server = "wagnerj.powwebmysql.com";
-	$username = "xanthakita";
-	$password = "6619ywkr";
+	$server = "xxx.xxx.xxx";
+	$username = "xxxxxxxxx";
+	$password = "xxxxxxxxxx";
 	$link = mysql_connect($server, $username, $password);
 	if (!$link) {
 	    die('Could not connect: ' . mysql_error());
